248745d54a9711c75c665ca0849fd89f5e521533
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-05-02 14:15:13 +0300

    Minor update

--- Main.lua
+++ Main.lua
@@ -1,8 +1,8 @@
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_LOGIN")
 
-frame:SetScript("OnEvent", function(self, eventName, ...)
-    if eventName == "PLAYER_LOGIN" then
-        ChatPlusDB = ChatPlusDB or {}
-    end
+frame:SetScript("OnEvent", function(self)
+    ChatPlusDB = ChatPlusDB or {}
+
+    self:UnregisterEvent("PLAYER_LOGIN")
 end)
